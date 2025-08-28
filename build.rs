f823ae111bc2f060f1a6b523048e0dab3def85a6
LeanSerra <46695152+LeanSerra@users.noreply.github.com>
2025-08-27 23:33:07 -0300

    feat: add rustemo for parsing

--- build.rs
+++ build.rs
@@ -1,9 +1,26 @@
-use std::path::Path;
+use rustemo_compiler::{LexerType, Settings};
+use std::path::PathBuf;
 
 fn main() {
-    let dest = Path::new("src").join("lex.rs");
-    let path = Path::new("src").join("lex.l");
-    if let Err(e) = rflex::process(path, Some(dest)) {
+    let root_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+
+    // Compile parser
+    let parser_rules = root_dir.join("src/grammar.rustemo");
+
+    if let Err(e) = Settings::new()
+        .force(false)
+        .out_dir_actions_root(root_dir.clone())
+        .out_dir_root(root_dir.clone())
+        .lexer_type(LexerType::Custom)
+        .process_grammar(&parser_rules)
+    {
+        eprintln!("Failed to compile rules: {e}");
+        std::process::exit(1)
+    }
+    // Compile Lexer
+    let lexer_out = root_dir.join("src/lex.rs");
+    let lexer_src = root_dir.join("src/lex.l");
+    if let Err(e) = rflex::process(lexer_src, Some(lexer_out)) {
         for cause in <dyn failure::Fail>::iter_chain(&e) {
             eprintln!("{}: {}", cause.name().unwrap_or("Error"), cause);
         }
