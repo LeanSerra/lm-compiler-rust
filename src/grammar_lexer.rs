6b869f9f142b56c9996cb84c42b7b107e5334d13
LeanSerra <46695152+LeanSerra@users.noreply.github.com>
2025-08-28 00:08:43 -0300

    feat: rustemo parser generator (#3)
    **Description**
    This PR adds rustemo as the parser generator implementing the following
    simple functionality:
    - Simple language definition in `grammar.rustemo` that allows parsing
    `hello_world.lm` example
    - Writing the output of the lexer to a file
    - Modifies `lex.l` to return rustemo tokens and implements the custom
    lexer to use rflex with rustemo
    - Writes the output of the parser to a file .parser with the rules it
    recognized

---
+++ src/grammar_lexer.rs
@@ -0,0 +1,56 @@
+use crate::grammar::{State, TokenKind};
+
+use rustemo::{Context, LRContext, Lexer, Location, Position, Token};
+use std::iter;
+
+/// We are parsing a slice of bytes.
+pub type Input = str;
+pub type Ctx<'i> = LRContext<'i, Input, State, TokenKind>;
+
+pub struct LexerAdapter();
+
+impl LexerAdapter {
+    pub fn new() -> LexerAdapter {
+        Self()
+    }
+}
+
+impl<'i> Lexer<'i, Ctx<'i>, State, TokenKind> for LexerAdapter {
+    type Input = Input;
+
+    fn next_tokens(
+        &self,
+        context: &mut Ctx<'i>,
+        input: &'i Self::Input,
+        _expected_tokens: Vec<(TokenKind, bool)>,
+    ) -> Box<dyn Iterator<Item = Token<'i, Self::Input, TokenKind>> + 'i> {
+        let mut pos = context.position();
+
+        let value;
+        let token;
+
+        if context.position() >= input.len() || input.is_empty() {
+            value = "";
+            token = TokenKind::STOP
+        } else {
+            let input = input.get(context.position()..input.len()).unwrap();
+            let mut lexer = crate::lex::Lexer::new(input);
+            token = lexer.yylex().unwrap_or(TokenKind::STOP);
+
+            let range = lexer.yytextpos();
+            pos += lexer.yytextpos().start;
+            value = unsafe { input.get_unchecked(range) };
+        }
+
+        context.set_position(pos);
+
+        Box::new(iter::once(Token {
+            kind: token,
+            value,
+            location: Location {
+                start: Position::Position(context.position()),
+                end: None,
+            },
+        }))
+    }
+}
