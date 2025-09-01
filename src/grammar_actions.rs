534aea34865ef6896a8d4cb83c59064ab35fa318
LeanSerra <46695152+LeanSerra@users.noreply.github.com>
2025-08-31 23:25:27 -0300

    feat: symbol table (#5)
    **Description**
    
    - implement function write_to_symbol_table for literals, and for
    variable declarations

--- src/grammar_actions.rs
+++ src/grammar_actions.rs
@@ -2,7 +2,9 @@
 /// All manual changes will be preserved except non-doc comments.
 use super::grammar::{Context, TokenKind};
 use super::grammar_lexer::Input;
-use crate::context::{write_to_lexer_file, write_to_parser_file};
+use crate::context::{
+    write_to_lexer_file, write_to_parser_file, write_to_symbol_table_file,
+};
 use rustemo::Token as RustemoToken;
 use std::fs::File;
 use std::io::Write;
@@ -171,6 +173,7 @@ pub fn var_declarations_var_declarations_single(
     _ctx: &Ctx,
     var_declaration: VarDeclaration,
 ) -> VarDeclarations {
+    var_declaration.write_to_symbol_table();
     write_to_parser_file("<VarDeclarations> -> <VarDeclaration>");
     VarDeclarations::VarDeclarationsSingle(var_declaration)
 }
@@ -179,6 +182,7 @@ pub fn var_declarations_var_declarations_recursive(
     var_declaration: VarDeclaration,
     var_declarations: VarDeclarations,
 ) -> VarDeclarations {
+    var_declaration.write_to_symbol_table();
     write_to_parser_file("<VarDeclarations> -> <VarDeclaration> <VarDeclaration>");
     VarDeclarations::VarDeclarationsRecursive(VarDeclarationsRecursive {
         var_declaration,
@@ -297,21 +301,28 @@ pub fn literal_integer_literal(
     token_int_literal: TokenIntLiteral,
 ) -> Literal {
     write_to_parser_file(&format!("<Literal> -> {token_int_literal}"));
-    Literal::IntegerLiteral(token_int_literal)
+    let literal = Literal::IntegerLiteral(token_int_literal);
+    literal.write_to_symbol_table();
+    literal
 }
 pub fn literal_float_literal(
     _ctx: &Ctx,
     token_float_literal: TokenFloatLiteral,
 ) -> Literal {
     write_to_parser_file(&format!("<Literal> -> {token_float_literal}"));
-    Literal::FloatLiteral(token_float_literal)
+    let literal = Literal::FloatLiteral(token_float_literal);
+    literal.write_to_symbol_table();
+    literal
 }
 pub fn literal_string_literal(
     _ctx: &Ctx,
     token_string_literal: TokenStringLiteral,
 ) -> Literal {
     write_to_parser_file(&format!("<Literal> -> {token_string_literal}"));
-    Literal::StringLiteral(token_string_literal)
+    /// We remove the "" from the string literal
+    let literal = Literal::StringLiteral(token_string_literal.replace("\"", ""));
+    literal.write_to_symbol_table();
+    literal
 }
 #[derive(Debug, Clone)]
 pub enum Data_Type {
@@ -330,4 +341,59 @@ pub fn data_type_float_type(_ctx: &Ctx, token_float: TokenFloat) -> Data_Type {
 pub fn data_type_string_type(_ctx: &Ctx, token_string: TokenString) -> Data_Type {
     write_to_parser_file(&format!("<Data_Type> -> {token_string}"));
     Data_Type::StringType(token_string)
+}
+impl Literal {
+    pub fn write_to_symbol_table(&self) {
+        match self {
+            Literal::IntegerLiteral(int) => {
+                write_to_symbol_table_file(
+                    &format!("{}|{}|{}|{}", int, "CONST_INT", int, int.len()),
+                )
+            }
+            Literal::FloatLiteral(float) => {
+                write_to_symbol_table_file(
+                    &format!("{}|{}|{}|{}", float, "CONST_FLOAT", float, float.len()),
+                )
+            }
+            Literal::StringLiteral(string) => {
+                write_to_symbol_table_file(
+                    &format!("{}|{}|{}|{}", string, "CONST_STRING", string, string.len()),
+                )
+            }
+        };
+    }
+}
+impl VarDeclaration {
+    pub fn write_to_symbol_table(&self) -> Data_Type {
+        match self {
+            Self::VarDeclarationSingle(single) => {
+                write_to_symbol_table_file(
+                    &format!(
+                        "{}|{}|{}|{}", single.token_id, single.data_type.to_string(),
+                        "-", single.token_id.len()
+                    ),
+                );
+                single.data_type.clone()
+            }
+            Self::VarDeclarationRecursive(recursive) => {
+                let data_type = recursive.var_declaration.write_to_symbol_table();
+                write_to_symbol_table_file(
+                    &format!(
+                        "{}|{}|{}|{}", recursive.token_id, data_type.to_string(), "-",
+                        recursive.token_id.len()
+                    ),
+                );
+                data_type
+            }
+        }
+    }
+}
+impl ToString for Data_Type {
+    fn to_string(&self) -> String {
+        match self {
+            Data_Type::IntType(_) => "VAR_INT".to_string(),
+            Data_Type::FloatType(_) => "VAR_FLOAT".to_string(),
+            Data_Type::StringType(_) => "VAR_STRING".to_string(),
+        }
+    }
 }
