1c61c325c76336911e3b497d8395fff5877f1abc
LeanSerra <46695152+LeanSerra@users.noreply.github.com>
2025-09-05 21:45:07 -0300

    feat: generate parsing,lexer and symbol table files for tests

--- src/tests/run.rs
+++ src/tests/run.rs
@@ -1,10 +1,18 @@
-use lm_compiler::{GrammarParser, LexerAdapter};
+use lm_compiler::{
+    GrammarParser, LexerAdapter, open_lexer_file, open_parser_file, open_symbol_table_file,
+    read_source_to_string, set_source_file_path,
+};
 use rustemo::Parser;
 use std::path::Path;
 
 fn integration_test(path: &Path) -> datatest_stable::Result<()> {
+    set_source_file_path(path.to_str().ok_or("Failed to get path")?);
+    open_lexer_file()?;
+    open_parser_file()?;
+    open_symbol_table_file()?;
+
     GrammarParser::new(LexerAdapter::new())
-        .parse(&std::fs::read_to_string(path).unwrap())
+        .parse(&read_source_to_string().map_err(|err| err.to_string())?)
         .map_err(|err| err.to_string().into())
         .map(|_program| ())
 }
